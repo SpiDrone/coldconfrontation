38dd6b11948a023122c575526898f688d688626c
joaoa <joaoa@DESKTOP-IH0MP7S>
2024-05-26 21:05:06 -0300

    MCreator Merge commit 'de80545a9ea881dcecb93282399cf39359f2358c' into 8d8c9fe60a7c91cda0ec08475259700f7739d5ea

--- src/main/java/net/mcreator/coldconfrontation/init/ColdconfrontationModTabs.java
+++ src/main/java/net/mcreator/coldconfrontation/init/ColdconfrontationModTabs.java
@@ -18,11 +18,6 @@ public class ColdconfrontationModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ColdconfrontationMod.MODID);
 	public static final RegistryObject<CreativeModeTab> TAB_COLD_CONFRONTATION = REGISTRY.register("tab_cold_confrontation",
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.coldconfrontation.tab_cold_confrontation")).icon(() -> new ItemStack(ColdconfrontationModBlocks.PINE_SAPLING.get())).displayItems((parameters, tabData) -> {
-				tabData.accept(ColdconfrontationModBlocks.PERMAFROSTED_TUNDRA_GRASS.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PERMAFROSTED_SOIL.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.TUNDRA_SHORT_GRASS.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_LEAVES.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_SAPLING.get().asItem());
 				tabData.accept(ColdconfrontationModBlocks.PINE_LOG.get().asItem());
 				tabData.accept(ColdconfrontationModBlocks.PINE_WOOD.get().asItem());
 				tabData.accept(ColdconfrontationModBlocks.STRIPPED_PINE_LOG.get().asItem());
@@ -36,14 +31,11 @@ public class ColdconfrontationModTabs {
 				tabData.accept(ColdconfrontationModBlocks.PINE_TRAPDOOR.get().asItem());
 				tabData.accept(ColdconfrontationModBlocks.PINE_PRESSURE_PLATE.get().asItem());
 				tabData.accept(ColdconfrontationModBlocks.PINE_BUTTON.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.SPRUCE_PALISADES.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.SPRUCE_PALISADE_BLOCK.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.SPRUCE_PALISADE_ROOF_STAIRS.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.SPRUCE_PALISADE_ROOF_SLAB.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_PALISADES.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_PALISADE_BLOCK.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_PALISADE_ROOF_STAIRS.get().asItem());
-				tabData.accept(ColdconfrontationModBlocks.PINE_PALISADE_ROOF_SLAB.get().asItem());
+				tabData.accept(ColdconfrontationModBlocks.PINE_LEAVES.get().asItem());
+				tabData.accept(ColdconfrontationModBlocks.PINE_SAPLING.get().asItem());
+				tabData.accept(ColdconfrontationModBlocks.PERMAFROSTED_SOIL.get().asItem());
+				tabData.accept(ColdconfrontationModBlocks.PERMAFROSTED_TUNDRA_GRASS.get().asItem());
+				tabData.accept(ColdconfrontationModBlocks.TUNDRA_SHORT_GRASS.get().asItem());
 			})
 
 					.build());
